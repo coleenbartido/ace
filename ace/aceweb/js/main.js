a5a4f2354989dc3e45ddfb8a949de7b66bc803c3
PPJao <ppj_ppj1@yahoo.com>
2017-03-17 16:34:27 +0800

    fix summary tab

--- ace/aceweb/js/main.js
+++ ace/aceweb/js/main.js
@@ -1,5 +1,5 @@
 //MAIN MODULE
-angular.module('aceWeb', ['aceWeb.controller','aceWeb.directive','ui.router','ngStorage','ngMessages', 'ui.bootstrap', 'ngTouch', 'ngAnimate','checklist-model','luegg.directives','angular.filter']) //kailangan yang ui.router pag gagamit ng $stateProvider. Yung 'aceWeb' naman is yung name ng module. Nakalink sya dun sa body tag ng index.html file which tells na yung module na 'aceWeb' ay e e implement sa html file na yun. Lahat ng may "app." ay under ng 'aceWeb' module
+angular.module('aceWeb', ['aceWeb.controller','aceWeb.directive','ui.router','ngStorage','ngMessages', 'ui.bootstrap', 'ngTouch', 'ngAnimate','checklist-model','luegg.directives','angular.filter','chart.js']) //kailangan yang ui.router pag gagamit ng $stateProvider. Yung 'aceWeb' naman is yung name ng module. Nakalink sya dun sa body tag ng index.html file which tells na yung module na 'aceWeb' ay e e implement sa html file na yun. Lahat ng may "app." ay under ng 'aceWeb' module
 
 
 // <------------------------------------------------------------------>
@@ -159,6 +159,7 @@ angular.module('aceWeb', ['aceWeb.controller','aceWeb.directive','ui.router','ng
     {
       url: '/summary',
       templateUrl: 'templates/admin/summary.html',
+      controller: 'SummaryController',
       loginRequired: true,
       accessLevel: 2
 
